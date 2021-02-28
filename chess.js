74c4e93e63c80d4327a7c603f6668346099a9e22
austin <austinisnorobot@gmail.com>
2021-02-28 18:27:40 -0500

    finish chess

--- chess.js
+++ chess.js
@@ -2,6 +2,7 @@ let size = 8;
 for (let i = 0; i< size; i++){
     let display = "";
     for (let j = 0; j < size; j++){
-        
+        display += (((i + j) % 2 == 0) ? " ": "#");
     }
+    console.log(display);
 }
