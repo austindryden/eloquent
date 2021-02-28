aac70af3702ca446a81df5191cb1cd9a827cd990
austin <austinisnorobot@gmail.com>
2021-02-28 18:15:41 -0500

    add fizz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+const fizz = 3;
+const buzz = 5;
+for (let i =0; i < 100; i++){
+    let display =" ";
+    if ((i % fizz) != 0 && (i % buzz) !=0)
+        display += i;
+    if (i% fizz == 0)
+        display += "Fizz";
+    if (i% buzz == 0)
+        display += "Buzz";
+    console.log(display);
+}
