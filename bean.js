a234f628ba00d3e9e9ca328225261b99551ed9b5
austin <austinisnorobot@gmail.com>
2021-03-01 21:13:53 -0500

    bean update

--- bean.js
+++ bean.js
@@ -6,4 +6,15 @@ function countBs(word){
     return count;
 }
 
+function countChar(word, letter){
+    let count = 0;
+    for (let i =0; i < word.length; i++){
+        if(word[i] == letter) count++;
+    }
+    return count;
+}
+
+
 console.log(countBs("BoBoBoB"));
+
+console.log(countChar("count the letter h in this sentence", "h"));
