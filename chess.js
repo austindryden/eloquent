8c3d4b8aae28af506e4f111c8378f7052b6d5f5b
austin <austinisnorobot@gmail.com>
2021-03-13 22:12:50 -0500

    makes into function

--- chess.js
+++ chess.js
@@ -1,9 +1,12 @@
 //prints a chess board of size, as defined by the variable size
-let size = 8;
-for (let i = 0; i< size; i++){
-    let display = "";
-    for (let j = 0; j < size; j++){
-        display += (((i + j) % 2 == 0) ? " ": "#");
+function chess(size){
+    for (let i = 0; i< size; i++){
+        let display = "";
+        for (let j = 0; j < size; j++){
+            display += (((i + j) % 2 == 0) ? " ": "#");
+        }
+        console.log(display);
     }
-    console.log(display);
 }
+
+chess(8);
