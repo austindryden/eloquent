d77e25b4c7a3b83a476ae8b42e192fcaa806a8ec
austin <austinisnorobot@gmail.com>
2021-03-14 22:34:10 -0400

    made into function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,17 @@
-//fizzbuzz classic
-const fizz = 3;
-const buzz = 5;
-for (let i =0; i < 100; i++){
-    let display =" ";
-    if ((i % fizz) != 0 && (i % buzz) !=0)
-        display += i;
-    if (i% fizz == 0)
-        display += "Fizz";
-    if (i% buzz == 0)
-        display += "Buzz";
-    console.log(display);
+//fizzbuzz classic, now in function form
+
+function fizzbuzz(total, fizz = 3, buzz = 5){
+
+    for (let i =0; i < total; i++){
+        let display =" ";
+        if ((i % fizz) != 0 && (i % buzz) !=0)
+            display += i;
+        if (i% fizz == 0)
+            display += "Fizz";
+        if (i% buzz == 0)
+            display += "Buzz";
+        console.log(display);
+    }
 }
+
+fizzbuzz(100);
