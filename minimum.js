85e28b63b696faf5373cff62e919ba30cc3f33f1
austin <austinisnorobot@gmail.com>
2021-03-17 18:39:31 -0400

    adds longer version

--- minimum.js
+++ minimum.js
@@ -1,5 +1,14 @@
 //arrow ternary minimum function
 const min = (x,y) => ((x<y) ? x:y);
 
+//long form minimum function.
+function min2(x,y){
+    if (x <= y){
+        return x;
+    }
+    return y;
+}
+
 console.log(min(2,3));
 console.log(min(40,70));
+
