453c213f5dee92e7b450ff308796c44f76b1488d
austin <austinisnorobot@gmail.com>
2021-03-18 16:14:22 -0400

    starts flatten

--- flatten.js
+++ flatten.js
@@ -1,1 +1,10 @@
-//Chapter five exercise
+//Take an array of arrays, and flatten into a single array, using reduce and concat
+
+function flatten(arr){
+    let newarr = [];
+    arr.reduce((a,b) => newarr.push(a));
+    
+    return newarr;
+}
+
+console.log(flatten([[1,2,3],[4,5,6]]));
