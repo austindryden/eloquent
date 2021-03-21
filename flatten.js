4f4c7aae0871a4b7f5b2f5aa8e70f0ff0e4c3393
austin <austinisnorobot@gmail.com>
2021-03-21 19:52:00 -0400

    flatten correct

--- flatten.js
+++ flatten.js
@@ -1,10 +1,7 @@
 //Take an array of arrays, and flatten into a single array, using reduce and concat
 
 function flatten(arr){
-    let newarr = [];
-    arr.reduce((a,b) => newarr.push(a));
-    
-    return newarr;
+       return arr.reduce((flat,current)=> flat.concat(current),[]);
 }
 
 console.log(flatten([[1,2,3],[4,5,6]]));
