40972d06b4c690eb4285882363d02794e0d6ddef
austin <austinisnorobot@gmail.com>
2021-03-20 21:55:59 -0400

    ideas

--- every.js
+++ every.js
@@ -1,6 +1,8 @@
 //returns true when given function returns true for every element in an array
 
 function every(arr,test){
-
+    for (int item of arr){
+        if (!test(item)) return false;
+    }
     return true;
 }
