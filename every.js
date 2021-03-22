a9252882c005344f1cc47492443378b916ce67ea
austin <austinisnorobot@gmail.com>
2021-03-21 20:06:08 -0400

    every correct

--- every.js
+++ every.js
@@ -1,8 +1,13 @@
 //returns true when given function returns true for every element in an array
 
 function every(arr,test){
-    for (int item of arr){
+    for (let item of arr){
         if (!test(item)) return false;
     }
     return true;
 }
+
+
+console.log(every([1, 3, 5], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
