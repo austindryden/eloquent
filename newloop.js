cf90e9ee56f0e9b421c78ded404b8d3da68051c4
austin <austinisnorobot@gmail.com>
2021-03-21 20:03:48 -0400

    new loop correct

--- newloop.js
+++ newloop.js
@@ -1,1 +1,17 @@
 //rebuild the for loop using higher-order functions.
+function loop(value, test, update, body){
+    while (test(value)){
+        body(value);
+        value = update(value);
+    }
+    return false;
+}
+
+//same function, built recursively.
+function loopr(value, test, update, body){
+    if (!test(value)) return false;
+    body(value);
+    loopr(update(value),test,update,body);
+}
+
+loopr(3, n => n > 0, n => n - 1, console.log);
