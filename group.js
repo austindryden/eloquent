baa103601573be749fe2781ea6411ed5f24e56ba
austin <austinisnorobot@gmail.com>
2021-03-24 19:53:26 -0400

    makes group iterable

--- group.js
+++ group.js
@@ -28,6 +28,27 @@ class Group{
         }
         return group;
     }
+
+    [Symbol.iterator](){
+        return new GroupIterator(this);
+    }
+    
+}
+
+class GroupIterator{
+    constructor(group){
+        this.group = group;
+        this.position = 0;
+    }
+
+    next(){
+        if (this.position == this.group.members.length) return {done: true};
+        //let value = {position : this.position, value: this.group.members[this.position]};
+        let value = this.group.members[this.position];
+        this.position++;
+        //return {value: value.value, done: false};
+        return {value, done: false};
+    }
 }
 
 let group = Group.from([10, 20]);
@@ -39,3 +60,10 @@ group.add(10);
 group.delete(10);
 console.log(group.has(10));
 // → false
+
+for (let value of Group.from(["a", "b", "c"])) {
+    console.log(value);
+  }
+  // → a
+  // → b
+  // → c
